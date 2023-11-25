2d62123ab627295cf917091fcb1fcebad9559d3a
szkye <thephantomszkye@gmail.com>
2023-11-25 20:29:59 +0100

    fixed long lines

--- orgopy.py
+++ orgopy.py
@@ -3,15 +3,16 @@ from pathlib import Path
 
 home = Path.home()
 
+
 def check_for_dirs():
-    
+
     global images_dir
     images_dir = Path(home, "Downloads", "IMAGES")
 
     global files_dir
     files_dir = Path(home, "Downloads", "FILES")
 
-    global apps_dir 
+    global apps_dir
     apps_dir = Path(home, "Downloads", "APPS")
 
     global misc_dir
@@ -19,12 +20,12 @@ def check_for_dirs():
 
     global music_dir
     music_dir = Path(home, "Downloads", "MUSIC")
-    
+
     if not images_dir.exists():
         os.mkdir(images_dir)
     else:
         print("Images folder already exists.")
-    
+
     if not files_dir.exists():
         os.mkdir(files_dir)
     else:
@@ -34,7 +35,7 @@ def check_for_dirs():
         os.mkdir(apps_dir)
     else:
         print("Apps folder already exists.")
-    
+
     if not misc_dir.exists():
         os.mkdir(misc_dir)
     else:
@@ -45,15 +46,18 @@ def check_for_dirs():
     else:
         print("All here")
 
+
 def move_by_type():
+
     downloads_dir = Path(home, "Downloads")
-    
     apps_suffix = ['.exe', '.sh', '.bat', '.run', '.apk', '.cmd', '.x86', '.x86_64', '.app']    
     music_suffix = ['.mp3', '.flac', '.asd', '.ogg', '.wav', '.wave', '.wma', '.aac']
     files_suffix = ['.txt', '.torrent', '.html', '.iso', '.bin']
-    image_suffix = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp', '.svg', '.ico', '.jfif', '.pjpeg', '.pjp', '.ai', '.eps', '.raw', '.indd', '.psd', '.pdf', '.xcf', '.dng', '.heif', '.jpe']
+    image_suffix = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp', '.svg', '.ico',
+                    '.jfif', '.pjpeg', '.pjp', '.ai', '.eps', '.raw', '.indd', '.psd', '.pdf',
+                    '.xcf', '.dng', '.heif', '.jpe']
     total_suffix = apps_suffix + music_suffix + files_suffix + image_suffix
-    
+
     for x in downloads_dir.iterdir():
         if x.is_file():
             if x.name == 'desktop.ini':
@@ -78,6 +82,7 @@ def move_by_type():
                 print(f'moved {x.name} to {misc_dir}.')
                 os.replace(downloads_dir/x.name, misc_dir/x.name)
                 continue
-   
+
+
 check_for_dirs()
 move_by_type()
